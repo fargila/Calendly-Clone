91cd7b50b73f2ffebcfd4e84da3c5aa2c1e48fde
Atila <atilahunesco@gmail.com>
2025-07-18 22:12:59 -0300

    feat: adicionando os componentes de navBar para entradas públicas e privadass.

--- constants/index.ts
+++ constants/index.ts
@@ -7,3 +7,22 @@ export const DAYS_OF_WEEK_IN_ORDER = [
     "saturday",
     "sunday"
 ] as const
+
+export const PrivateNavLinks = [
+    {
+        imgUrl: '/assest/events.svg',
+        route: '/events',
+        label: 'My Events'
+    },
+    {
+        imgUrl: '/assest/schedule.svg',
+        route: '/schedule',
+        label: 'My Schedules'
+    },
+    {
+        imgUrl: '/assest/public.svg',
+        route: '/book',
+        label: 'Public Profile'
+    },
+    
+] as const
